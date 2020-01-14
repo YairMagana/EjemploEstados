d138dd9cbcb3816fc8e9524469a26c98d24d7f8c
Yair Jafet Magaña Chan <yair.magana@blueoceantech.com.mx>
2020-01-13 18:31:43 -0600

    Initial commit

---
+++ EjemploEstados/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjemploEstados
+{
+    public class Alarma
+    {
+        public IEstadoAlarma estadoAlarma;
+
+        public Alarma(IEstadoAlarma estadoAlarma)
+        {
+            this.estadoAlarma = estadoAlarma;
+        }
+
+        public void EstablecerEstado(IEstadoAlarma estadoAlarma)
+        {
+            this.estadoAlarma = estadoAlarma;
+        }
+
+        public void Activar()
+        {
+            estadoAlarma.Activar();
+        }
+
+        public void Desactivar()
+        {
+            estadoAlarma.Desactivar();
+        }
+    }
+
+    public interface IEstadoAlarma
+    {
+        void Activar();
+        void Desactivar();
+    }
+
+    public class EstadoAlarmaActiva : IEstadoAlarma
+    {
+        private Alarma alarma;
+
+        public void Activar()
+        {
+            Console.WriteLine("La alarma ya está activa");
+        }
+
+        public void Desactivar()
+        {
+            Console.WriteLine("DESACTIVANDO alarma");
+            alarma.Desactivar();
+        }
+
+        public void EstablecerContexto()
+        {
+
+        }
+    }
+
+    public class EstadoAlarmaInactiva : IEstadoAlarma
+    {
+        private Alarma alarma;
+
+        public void Activar()
+        {
+            Console.WriteLine("ACTIVANDO alarma");
+            alarma.Activar();
+        }
+
+        public void Desactivar()
+        {
+            Console.WriteLine("La alarma ya está desactivada");
+        }
+
+        public void EstablecerContexto()
+        {
+
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Alarma alarma = new Alarma(new EstadoAlarmaInactiva());
+
+            alarma.EstablecerEstado(new EstadoAlarmaActiva());
+            Console.ReadKey();
+        }
+    }
+}
